2cfb47a24b1e1824635b6291d2ecd3ae887d14d7
Rafał Zając <rf.zajac@gmail.com>
2024-01-24 18:25:02 +0100

    reviewed chooseRover function and added some basic documentation

--- src/Utility/ChooseRover.ts
+++ src/Utility/ChooseRover.ts
@@ -1,19 +1,29 @@
 import { responseManifest } from '../types/fetchedTypes.js';
 import * as RoverDesc from './DisplayRoverInfo.js';
-// ? ----------------------------------------------
-// ? SELECT PARAMETERS AND FETCH MISSION MANIFEST
-// ? ----------------------------------------------
+
+/**
+  It queries select field on the page containing a string with a name
+  of a rover to display data for. In case none was selected it will trigger
+  a function do display error with a message provided manually. If it is selected
+  properly it will fetch data from NASA API's mission manifest containing information
+  describing selected rover's mission and pass it to a function that will display it 
+  on the page 
+*/
 export function chooseRover() {
    const roverSelect = document.querySelector(
       '#rover-select'
    ) as HTMLSelectElement;
+
+   // * Listen to changes in select field and store seleted value in a variable
    roverSelect.addEventListener('change', () => {
       const roverName = roverSelect.value;
-      console.log(roverName);
+
+      // * In case nothing was selected display an error
       if (roverName === '') {
          RoverDesc.displayEmptyRoverErr(
             'Nothing to display! Please select a rover!'
          );
+         // * If rover was selected fetch data from its mission manifest entry
       } else {
          fetch(
             `https://api.nasa.gov/mars-photos/api/v1/manifests/${roverName}/?api_key=wlcQTmhFQql1kb762xbFcrn8imjFFLumfDszPmsi`
