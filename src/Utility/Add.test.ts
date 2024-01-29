c388daa615fc3414e1450a200b8bb40d2d486938
Rafał Zając <rf.zajac@gmail.com>
2024-01-29 19:15:20 +0100

    added first simple test to play with

--- src/Utility/Add.test.ts
+++ src/Utility/Add.test.ts
@@ -1,3 +1,24 @@
-import { it } from 'vitest'
+import { expect, it } from 'vitest'
+import { add } from './Add'
 
-it('')
+it('Should add two numbers and return a sum', () => {
+  //   Arrange
+  const valA = 2
+  const valB = 4
+
+  //   Act
+  const sum = add(valA, valB)
+
+  //   Assert
+  const expectedResult = valA + valB
+  expect(sum).toBe(expectedResult)
+})
+
+it('Should yield a Nan value as a result if at least one value is of wrong type', () => {
+  const valA = 'hello'
+  const valB = 4
+
+  const sum = add(valA, valB)
+
+  expect(sum).toBeNaN()
+})
