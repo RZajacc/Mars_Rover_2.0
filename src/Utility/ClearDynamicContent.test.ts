c67cf10851b734715aa78013789a47da22993e42
Rafał Zając <rf.zajac@gmail.com>
2024-02-01 20:34:20 +0100

    added some tests to a function cleaning dynamic content

--- src/Utility/ClearDynamicContent.test.ts
+++ src/Utility/ClearDynamicContent.test.ts
@@ -3,7 +3,10 @@ import path from 'path'
 
 import { it, describe, expect, vi, beforeEach } from 'vitest'
 
-import { removeAllChildNodes } from './ClearDynamicContent'
+import {
+  removeAllChildNodes,
+  cleanAllDynamicContent
+} from './ClearDynamicContent'
 import { Window } from 'happy-dom'
 
 const htmlDocPath = path.join(process.cwd() + '/public', 'content.html')
@@ -18,12 +21,38 @@ beforeEach(() => {
   document.write(htmlDocumentContent)
 })
 
-it('Should remove all childnodes', () => {
-  const roverInfo = document.querySelector(
-    '#rover-info'
-  )! as unknown as HTMLDivElement
-  removeAllChildNodes(roverInfo)
-  const childEl = roverInfo.firstElementChild
+describe('removeAllChildNodes', () => {
+  it('Should not throw an error if provided element doesnt contain any childnodes initially', () => {
+    const roverInfo = document.querySelector(
+      '#rover-info'
+    )! as unknown as HTMLDivElement
+    removeAllChildNodes(roverInfo)
+    const childEl = roverInfo.firstElementChild
 
-  expect(childEl).toBeNull()
+    expect(childEl).toBeNull()
+  })
+  it('Should remove all childnodes from a given HTMLElement', () => {
+    const roverInfo = document.querySelector(
+      '#rover-info'
+    )! as unknown as HTMLDivElement
+    const pEl = document.createElement('p') as unknown as HTMLParagraphElement
+    roverInfo.appendChild(pEl)
+
+    removeAllChildNodes(roverInfo)
+    const childEl = roverInfo.firstElementChild
+
+    expect(childEl).toBeNull()
+  })
+})
+
+describe('cleanAllDynamicContent()', () => {
+  it('Make it one by one or for randomly selected one', () => {})
+  it('Should change camInfo paragraph to contain an empty string', () => {
+    cleanAllDynamicContent()
+    const camInfo = document.querySelector(
+      '#cameras-info'
+    )! as unknown as HTMLParagraphElement
+
+    expect(camInfo.innerHTML).toBe('')
+  })
 })
