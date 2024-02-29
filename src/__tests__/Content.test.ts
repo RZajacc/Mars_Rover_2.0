f84a610356218435fc750faffd3b87b7e4d7a539
Rafał Zając <rf.zajac@gmail.com>
2024-02-29 11:29:39 +0100

    added test file for Content module. Test will cover only 2 fetching functions

---
+++ src/__tests__/Content.test.ts
@@ -0,0 +1,20 @@
+import fs from 'fs'
+import path from 'path'
+
+import { Window } from 'happy-dom'
+import { beforeEach, vi } from 'vitest'
+
+import { fetchBasic } from '../content'
+
+const htmlDocPath = path.join(process.cwd(), 'public', 'content.html')
+const htmlDocumentContent = fs.readFileSync(htmlDocPath).toString()
+
+const window = new Window()
+const document = window.document
+vi.stubGlobal('document', document)
+
+beforeEach(() => {
+  document.body.innerHTML = ''
+  document.write(htmlDocumentContent)
+  vi.clearAllMocks()
+})
