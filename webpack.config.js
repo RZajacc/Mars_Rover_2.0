4605be055b95384499161827dfa6b47cb530a74e
RZajacc <rf.zajac@gmail.com>
2024-03-25 19:55:25 +0100

    adjusted html code to contain more semantic elements in index page

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,33 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
 module.exports = {
   mode: 'development',
   entry: {
     content: './src/content.ts',
     contact: './src/contact.ts'
   },
+  plugins: [new MiniCssExtractPlugin()],
   module: {
     rules: [
       {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.ts$/,
         use: 'ts-loader',
         exclude: '/node_modules/'
       }
     ]
   },
+
   resolve: {
     extensions: ['.ts', '.js']
   },
   output: {
     // filename: 'contact.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'assets', 'scripts'),
+    publicPath: 'assets/scripts/'
   }
 }
